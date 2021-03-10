d9e00d9efa3901751cc78400f15fc41e3ec92ff9
Kasra Hosseini <khosseini@turing.ac.uk>
2021-03-10 17:43:29 +0000

    add ctgan

--- privgem/__init__.py
+++ privgem/__init__.py
@@ -1,3 +1,4 @@
 from privgem.tabular.patectgan import patectgan as tabular_patectgan
 from privgem.tabular.dpctgan import dpctgan as tabular_dpctgan
+from privgem.tabular.ctgan import ctgan as tabular_ctgan
 from privgem.tabular import utils as tabular_utils
