50a631645211b0c2116303d994fee9dd444ac344
kasra-hosseini <khosseini@turing.ac.uk>
2021-03-10 14:39:47 +0000

    add dpctgan method, note that, this function is from smartnoise library

--- privgem/__init__.py
+++ privgem/__init__.py
@@ -1,2 +1,3 @@
 from privgem.tabular.patectgan import patectgan as tabular_patectgan
+from privgem.tabular.dpctgan import dpctgan as tabular_dpctgan
 from privgem.tabular import utils as tabular_utils
