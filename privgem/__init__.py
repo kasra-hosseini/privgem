f9274f7befe9f8e3ba4354dc6387d53e3329019f
kasra-hosseini <khosseini@turing.ac.uk>
2021-06-18 13:52:48 +0100

    update __init__

--- privgem/__init__.py
+++ privgem/__init__.py
@@ -3,3 +3,5 @@ from privgem.tabular.dpctgan import dpctgan as tabular_dpctgan
 from privgem.tabular.ctgan import ctgan as tabular_ctgan
 from privgem.tabular import utils as tabular_utils
 from privgem.tabular import metrics as tabular_metrics
+from privgem.tabular import artificial_data as tabular_artificial
+from privgem.tabular.ppgm.private_pgm import PPGM as tabular_ppgm
