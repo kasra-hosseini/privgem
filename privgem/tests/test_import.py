ccd7bdf537b75e407338963665bffed26996932d
kasra-hosseini <khosseini@turing.ac.uk>
2021-06-18 14:03:57 +0100

    udpate test import

--- privgem/tests/test_import.py
+++ privgem/tests/test_import.py
@@ -2,3 +2,9 @@ import pytest
 
 def test_import():
     from privgem import tabular_patectgan
+    from privgem import tabular_dpctgan
+    from privgem import tabular_ctgan
+    from privgem import tabular_utils
+    from privgem import tabular_metrics
+    from privgem import tabular_artificial
+    from privgem import tabular_ppgm
